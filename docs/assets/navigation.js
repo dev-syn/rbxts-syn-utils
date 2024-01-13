d4378e6848da4c9c7eb93d464ed9e2e7a79fe516
Casey <103451081+dev-syn@users.noreply.github.com>
2024-01-13 02:39:36 -0500

    Build docs

---
+++ docs/assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAACouuVipJrShRslIK9dR3zs8rKcrPyUktKtb3SyxLSixCiCjpKBUklmQAFebmp5TmpBbrh3rGI2mIR9egl1GSmwPUlZ2Zl6JkZaSjlJyRmZNSlJqnZBUNtzS1oiC/qMQWYXhyTmJxMRGGQzTGo1piaGRRG1sbCwBya6Oj1AAAAA=="
